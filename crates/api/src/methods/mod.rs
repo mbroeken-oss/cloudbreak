afe44c5982e15acd8e5d73c0f50c63df61296235
Arthur Paiva <arthur001123@gmail.com>
2026-06-22 11:26:36 +0000

    Implement TokenAccountState filter

--- crates/api/src/methods/mod.rs
+++ crates/api/src/methods/mod.rs
@@ -102,7 +102,10 @@ impl<'a> SqlDataSliceFilter<'a> {
                     hex::encode(bytes)
                 ))
             }
-            RpcFilterType::TokenAccountState => None,
+            RpcFilterType::TokenAccountState => Some(format!(
+                "(length({0}.data) = 165 OR (length({0}.data) > 165 AND SUBSTRING({0}.data FROM 166 FOR 1) = E'\\\\x02'::bytea))",
+                self.table
+            )),
         }
     }
 }
